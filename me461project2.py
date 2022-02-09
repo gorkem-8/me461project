73cfe7841afb1f36366e353ae7be1f321b01491e
gorkem-8 <84041501+gorkem-8@users.noreply.github.com>
2022-02-09 23:23:24 +0300

    Update me461project2.py

--- me461project2.py
+++ me461project2.py
@@ -5,7 +5,7 @@ class backspacex:
         self.name = userName # your object will be given a user name, i.e. your group name
         self.maxStep = maxStepSize # maximum length of the returned path from run()
         self.maxTime = maxTime # run() is supposed to return before maxTime
-
+                                                                                                                                                                                                                
     def run(self, img, info):
         myinfo = info[self.name]
 
