2540a05b8b19250208932ef1f4c8c04eadf3b00a
gorkem-8 <84041501+gorkem-8@users.noreply.github.com>
2022-02-10 17:29:42 +0300

    Update me461project3.py

--- me461project3.py
+++ me461project3.py
@@ -15,7 +15,6 @@ class backspacex:
         goal=[[0,0],[0,0]]
         for i in range(1,9):
             if i==1:
-                goal==[[y,x-50],[y+50,x-50]]
+                goal2==[[y,x-50],[y+50,x-50]]
 
-        
-        return goal
+        return goal2
